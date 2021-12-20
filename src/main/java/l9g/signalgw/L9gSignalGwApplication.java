9b55ece193cf09b3f214748249eca97221841b46
Thorsten Ludewig <th@l9g.de>
2021-12-20 16:52:26 +0100

    reformat

--- src/main/java/l9g/signalgw/L9gSignalGwApplication.java
+++ src/main/java/l9g/signalgw/L9gSignalGwApplication.java
@@ -3,11 +3,15 @@ package l9g.signalgw;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author Thorsten Ludewig (t.ludewig@gmail.com)
+ */
 @SpringBootApplication
-public class L9gSignalGwApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(L9gSignalGwApplication.class, args);
-	}
-
+public class L9gSignalGwApplication
+{
+  public static void main(String[] args)
+  {
+    SpringApplication.run(L9gSignalGwApplication.class, args);
+  }
 }
