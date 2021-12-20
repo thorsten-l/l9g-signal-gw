985ff659d1c1e6f41377a35a963573b1f706d7c8
Thorsten Ludewig <th@l9g.de>
2021-12-20 21:02:47 +0100

    main throws exception

--- src/main/java/l9g/signalgw/L9gSignalGwApplication.java
+++ src/main/java/l9g/signalgw/L9gSignalGwApplication.java
@@ -21,7 +21,7 @@ public class L9gSignalGwApplication
 
   private final static Options OPTIONS = new Options();
 
-  public static void main(String[] args)
+  public static void main(String[] args) throws Exception
   {
 
     CmdLineParser parser;
