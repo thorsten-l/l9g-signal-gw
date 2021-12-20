1699d41e1b9ddd2000ecd332424ee8e2c289e168
Thorsten Ludewig <th@l9g.de>
2021-12-20 17:21:35 +0100

    /api/build shows build properties

--- src/main/java/l9g/signalgw/L9gSignalGwApplication.java
+++ src/main/java/l9g/signalgw/L9gSignalGwApplication.java
@@ -1,17 +1,28 @@
 package l9g.signalgw;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * 
+ *
  * @author Thorsten Ludewig (t.ludewig@gmail.com)
  */
 @SpringBootApplication
 public class L9gSignalGwApplication
 {
+  private final static Logger LOGGER = LoggerFactory.getLogger(
+    L9gSignalGwApplication.class.getName());
+
   public static void main(String[] args)
   {
+    BuildProperties build = BuildProperties.getInstance();
+    LOGGER.info("Project Name    : " + build.getProjectName());
+    LOGGER.info("Project Version : " + build.getProjectVersion());
+    LOGGER.info("Build Timestamp : " + build.getTimestamp());
+    LOGGER.info("Build Profile   : " + build.getProfile() + "\n");
+
     SpringApplication.run(L9gSignalGwApplication.class, args);
   }
 }
