5c5d9b3d59397b6145437dd7a935e26a474c8cb9
Thorsten Ludewig <th@l9g.de>
2021-12-20 21:03:25 +0100

    starting signal handler

---
+++ src/main/java/l9g/signalgw/handler/SignalHandler.java
@@ -0,0 +1,65 @@
+package l9g.signalgw.handler;
+
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Thorsten Ludewig (t.ludewig@gmail.com)
+ */
+public class SignalHandler implements Runnable
+{
+  private final static Logger LOGGER = LoggerFactory.getLogger(
+    SignalHandler.class.getName());
+
+  private final static SignalHandler SINGLETON = new SignalHandler();
+
+  private SignalHandler()
+  {
+    handlerThread = new Thread(this);
+    handlerThread.setName("SignalHandler");
+    handlerThread.setDaemon(true);
+  }
+
+  public static SignalHandler getInstance()
+  {
+    if (!SINGLETON.handlerThread.isAlive())
+    {
+      LOGGER.debug("Starting Handler");
+      SINGLETON.handlerThread.start();
+    }
+    return SINGLETON;
+  }
+
+  public void sendMessage(String message)
+  {
+    sendingQueue.add(message);
+  }
+
+  @Override
+  public void run()
+  {
+    while (true)
+    {
+      try
+      {
+        String message = sendingQueue.poll();
+        if ( message != null )
+        {
+          LOGGER.debug(message);
+        }
+        Thread.sleep(100);
+      }
+      catch (InterruptedException ex)
+      {
+        LOGGER.error("error", ex);
+      }
+    }
+  }
+
+  private final Thread handlerThread;
+
+  private final Queue<String> sendingQueue = new LinkedBlockingQueue<>();
+}
