6fdc87a1cba1c6fb2625a9c4d0490cf212914582
Thorsten Ludewig <th@l9g.de>
2021-12-26 09:40:56 +0100

    rename form to template

---
+++ src/main/java/l9g/signalgw/controller/SendController.java
@@ -0,0 +1,58 @@
+package l9g.signalgw.controller;
+
+import com.google.common.base.Strings;
+import javax.servlet.http.HttpServletRequest;
+import l9g.signalgw.BuildProperties;
+import l9g.signalgw.Config;
+import l9g.signalgw.handler.SignalHandler;
+import l9g.signalgw.handler.SignalMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Thorsten Ludewig (t.ludewig@gmail.com)
+ */
+@RestController
+public class SendController
+{
+  private final static Logger LOGGER = LoggerFactory.getLogger(SendController.class.getName());
+
+  @GetMapping(path = "/api/send", produces = MediaType.TEXT_PLAIN_VALUE)
+  public String handleSendMessage(HttpServletRequest request, 
+    @RequestParam(name = "c", required = false) String clientName,
+    @RequestParam(name = "m", required = false) String messageText,
+    @RequestParam(name = "t", required = false) String messageTemplate
+  )
+  {
+    LOGGER.debug("handleSendMessage");
+
+    String remoteAddr = request.getHeader("X-Forward-For");
+    if (remoteAddr == null)
+    {
+      remoteAddr = request.getRemoteAddr();
+    }
+
+    String remoteHost = Strings.isNullOrEmpty(clientName)
+      ? request.getRemoteHost()
+      : clientName;
+    
+    String text = Strings.isNullOrEmpty(messageText)
+      ? ""
+      : messageText;
+    
+    String template = Strings.isNullOrEmpty(messageTemplate)
+      ? "default"
+      : messageTemplate;
+    
+    SignalHandler.getInstance().sendMessage(new SignalMessage(Config.
+      getInstance().getDefaultSignalReceipient(), true, 
+      text, remoteHost, remoteAddr, template ));
+
+    return "OK";
+  }
+}
