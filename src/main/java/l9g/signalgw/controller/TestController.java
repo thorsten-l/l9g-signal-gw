840da82b2b3a20b2876f35f947a0907f11749437
Thorsten Ludewig <th@l9g.de>
2021-12-26 09:43:04 +0100

    add parameters

--- src/main/java/l9g/signalgw/controller/TestController.java
+++ src/main/java/l9g/signalgw/controller/TestController.java
@@ -1,5 +1,7 @@
 package l9g.signalgw.controller;
 
+import com.google.common.base.Strings;
+import javax.servlet.http.HttpServletRequest;
 import l9g.signalgw.BuildProperties;
 import l9g.signalgw.Config;
 import l9g.signalgw.handler.SignalHandler;
@@ -8,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -21,14 +24,31 @@ public class TestController
     TestController.class.getName());
 
   @GetMapping(path = "/api/test", produces = MediaType.APPLICATION_JSON_VALUE)
-  public BuildProperties handleTest()
+  public BuildProperties handleTest(HttpServletRequest request, 
+    @RequestParam(name = "c", required = false) String clientName,
+    @RequestParam(name = "m", required = false) String messageText
+  )
   {
     LOGGER.debug("test");
+
+    String remoteAddr = request.getHeader("X-Forward-For");
+    if (remoteAddr == null)
+    {
+      remoteAddr = request.getRemoteAddr();
+    }
+
+    String remoteHost = Strings.isNullOrEmpty(clientName)
+      ? request.getRemoteHost()
+      : clientName;
+    
+    String text = Strings.isNullOrEmpty(messageText)
+      ? "API test"
+      : messageText;
     
     SignalHandler.getInstance().sendMessage(new SignalMessage(Config.
-      getInstance().getDefaultSignalReceipient(), true,
-      "Test " + System.currentTimeMillis()));
-    
+      getInstance().getDefaultSignalReceipient(), true, 
+      text, remoteHost, remoteAddr ));
+
     return BuildProperties.getInstance();
   }
 }
